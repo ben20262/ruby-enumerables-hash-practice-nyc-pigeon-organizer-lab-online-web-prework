76b3e684fbcda3ea08f2db4b0895427bfc4f9a6e
Benjamin Small <ben20262@aim.com>
2019-11-17 19:00:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_bois = {}
+  names = []
+  data.each_value do |att|
+    att.each_value do |spec_value|
+      names.concat(spec_value)
+    end
+  end
+  names.uniq!
+  names.each do |bird|
+    pigeon_bois[bird] = attributes(data, bird)
+  end
+  pigeon_bois
+end
+
+def attributes(data, name)
+  hash = {}
+  pigeon = {}
+  data.each_pair do |att_key, att_value|
+    spec = []
+    att_value.each_pair do |spec_key, spec_value|
+      if spec_value.include?(name)
+        spec << spec_key.to_s
+      end
+    end
+    pigeon[att_key] = spec
+  end
+  pigeon
 end
