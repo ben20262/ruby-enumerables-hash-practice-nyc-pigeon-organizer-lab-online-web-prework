3a6de3d99bac2530a5f00437380639ee6357fb95
Benjamin Small <ben20262@aim.com>
2019-11-17 17:49:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,17 @@ def nyc_pigeon_organizer(data)
   keys1 = []
   keys2 = []
   keys1 = data.keys
+  x = 0
+  y = 0
   data.each do |att|
     keys2 << att.keys
     att.each do |spec|
+      keys2[x][y].to_s
       names.concat(spec.values)
     end
+  end
+  names = names.uniq
+  names.each do |name|
+    pigeon_bois[name] = {}
   end
 end
