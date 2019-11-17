2ca7493f97a31616a5c8f8290e99d094ade978f6
Benjamin Small <ben20262@aim.com>
2019-11-17 17:14:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,6 @@ def nyc_pigeon_organizer(data)
   pigeon_bois = {}
   magic = data.keys
   data.each do |att_key, att_value|
-    mana = attribute.keys
     attribute.each do |spec_key, spec_value|
       spec_value.each do |name|
         if !pigeon_bois.has_key?(name)
