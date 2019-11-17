392aabd93e67d779edf21b780898e7347615c296
Benjamin Small <ben20262@aim.com>
2019-11-17 17:13:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,19 @@
 def nyc_pigeon_organizer(data)
   pigeon_bois = {}
   magic = data.keys
-  data.each do |attribute|
+  data.each do |att_key, att_value|
     mana = attribute.keys
-    attribute.each do |spec|
-      names = spec.values
-      names.each do |name|
+    attribute.each do |spec_key, spec_value|
+      spec_value.each do |name|
         if !pigeon_bois.has_key?(name)
           magic.each do |key|
             pigeon_bois[name] = {key => []}
+            
           end
+          
         end
       end
     end
   end
+  puts pigeon_bois
 end
